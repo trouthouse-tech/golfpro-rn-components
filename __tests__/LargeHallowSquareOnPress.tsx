1685c09eb20728f9d420ed4bf3c36110f731bf39
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-04 19:56:30 +0100

    tests for LargeHallowSquareOnPress

---
+++ __tests__/LargeHallowSquareOnPress.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {LargeHallowSquareOnPress} from "../src/Buttons";
+import {Colors} from '../src/styles/';
+
+
+import { render, cleanup, fireEvent } from "@testing-library/react-native";
+import '@testing-library/jest-native/extend-expect';
+import renderer from 'react-test-renderer';
+
+describe('LargeHallowSquareOnPress', () => {
+
+  const mockClickFunction = jest.fn();
+
+  afterEach(cleanup);
+  test('renders and matches snapshot', () => {
+    const tree = renderer.create(<LargeHallowSquareOnPress text="Test" onPress={mockClickFunction} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a clickable LargeHallowSquareOnPress", () => {
+    const { getByText } = render(
+      <LargeHallowSquareOnPress text="Clickable" onPress={mockClickFunction}/>
+    );
+  
+    const button = getByText("Clickable");
+    fireEvent.press(button); //press the button
+    expect(mockClickFunction).toHaveBeenCalledTimes(1); 
+  });
+
+  it("styles LargeHallowSquareOnPress correctly", () => {
+    const { getByTestId } = render(
+      <LargeHallowSquareOnPress text="Clickable" onPress={mockClickFunction} 
+           />
+    ); 
+    const baseButton = getByTestId("basebutton");
+    expect(baseButton).toHaveStyle({
+      backgroundColor: Colors.white,
+      borderColor: Colors.main_green
+    });
+  });
+
+});
+
