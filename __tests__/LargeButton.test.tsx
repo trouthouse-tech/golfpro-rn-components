bf16948fecb8e271cef73b0be26bbdf3df843fef
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-04 18:40:37 +0100

    test if largeButton is clickable.

--- __tests__/LargeButton.test.tsx
+++ __tests__/LargeButton.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {StyleSheet, Text} from 'react-native';
 import {LargeButton} from "../src/Buttons/";
 
+import { render, cleanup, fireEvent } from "@testing-library/react-native";
+import '@testing-library/jest-native/extend-expect';
 import renderer from 'react-test-renderer';
 
-test('renders correctly', () => {
-  const tree = renderer.create(<LargeButton text="Test" onPress={() => {}} />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('LargeButton', () => {
+
+  const mockClickFunction = jest.fn();
+
+  afterEach(cleanup);
+  test('renders and matches snapshot', () => {
+    const tree = renderer.create(<LargeButton text="Test" onPress={mockClickFunction} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a clickable LargeButton", () => {
+    const { getByText } = render(
+      <LargeButton text="Clickable" onPress={mockClickFunction}/>
+    );
+  
+    const button = getByText("Clickable");
+    fireEvent.press(button); //press the button
+    expect(mockClickFunction).toHaveBeenCalledTimes(1); 
+  });
+
 });
+
