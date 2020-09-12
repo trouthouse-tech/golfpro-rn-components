e36b6d6ffb8884e33cccd9d8621f1c82c28455e5
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-12 17:28:35 +0100

    Extracted inline button styling into customStyles const largeSquare style prop is largeButton

--- src/Buttons/LargeButton/index.tsx
+++ src/Buttons/LargeButton/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
-import {Fonts, Utils, Colors} from '../../styles/';
+import { StyleSheet, Text } from 'react-native';
+import { Fonts, Utils, Colors } from '../../styles/';
 import BaseButton from '../BaseButton';
-import {ButtonProps} from '../';
+import { ButtonProps } from '../';
 
 export default function LargeButton({
   onPress,
@@ -12,29 +12,28 @@ export default function LargeButton({
   borderColor,
 }: ButtonProps) {
   const child = (
-    <Text style={[styles.buttonText, {color: textColor || Colors.white}]}>
+    <Text style={[styles.buttonText, { color: textColor || Colors.white }]}>
       {text}
     </Text>
   );
+
+  const customStyles = {
+    backgroundColor: buttonColor || Colors.main_green,
+    borderColor: borderColor || Colors.main_green,
+    borderWidth: 1,
+  };
+
   return (
     <BaseButton
       onPress={() => onPress()}
-      buttonStyle={[
-        styles.largeSquare,
-        // eslint-disable-next-line react-native/no-inline-styles
-        {
-          backgroundColor: buttonColor || Colors.main_green,
-          borderColor: borderColor || Colors.main_green,
-          borderWidth: 1,
-        },
-      ]}
+      buttonStyle={[styles.largeButton, customStyles]}
       child={child}
     />
   );
 }
 
 const styles = StyleSheet.create({
-  largeSquare: {
+  largeButton: {
     width: Utils.DEVICE_WIDTH / 1.2,
     height: Utils.DEVICE_HEIGHT / 12,
     borderRadius: 5,
